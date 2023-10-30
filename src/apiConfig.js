0600a2a3381ea923918d377727c7aa95b48b34ac
Etancher <30360093+jahanvir@users.noreply.github.com>
2023-10-30 23:41:08 +0530

    removed api from config file

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,5 @@
 export const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
-export const frontendApiBaseUrl = apiBaseUrl + '/api/front-end';
+export const frontendApiBaseUrl = apiBaseUrl + '/front-end';
 
 
 
